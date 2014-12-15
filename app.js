93d99c237d7a516b21cad1d61828fa6718ceeb7a
amagitakayosi <fand@gmork.in>
2014-12-15 21:39:33 +0900

    ok

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+var FPS = 30;
+var d = 1000 / 30.0;
+
 $(function () {
   if (!Glsl.supported()) alert("WebGL is not supported.");
   var glsl = Glsl({
     canvas: document.getElementById("viewport"),
     fragment: document.getElementById("fragment").textContent,
     variables: {
-      time: 0 // The time in ms
+      time: 0, // The time in ms
+      mouse: [0, 0]
     },
     update: function (time) {
       this.set("time", time);
